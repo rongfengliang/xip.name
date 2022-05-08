e6fbf743d9c067171de1f0536c67fe4202fb88b5
rong fengliang <1141591465@qq.com>
2022-05-08 12:32:25 +0800

    add more

--- app.sh
+++ app.sh
@@ -4,5 +4,5 @@ GOOS=linux CGO_ENABLED=0 go build
 
 fpm -s dir -t rpm -n xip.name --rpm-os linux -v v1.0-centos7 \
   ./xip.name=/usr/bin/xip.name \
-  ./xip.name.service=/usr/lib/systemd/xip.name.service \
-  ./xip.name.conf=/etc/xip/xip.name.conf
+  ./xip.service=/usr/lib/systemd/xip.service \
+  ./xip.conf=/etc/xip/xip.conf
